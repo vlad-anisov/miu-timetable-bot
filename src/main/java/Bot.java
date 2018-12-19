006dd814936c237e3417b2bda7156eff7302cf9f
Vlad Anisov <anisoffgo@gmail.com>
2018-12-19 12:55:08 +0300

    123
    122

---
+++ src/main/java/Bot.java
@@ -0,0 +1,28 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        SendMessage message = new SendMessage();
+        Schedule schedule = new Schedule();
+        //message.setText(update.getMessage().getText());
+        message.setText(schedule.Schedule());
+        message.setChatId(update.getMessage().getChatId());
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBotUsername() {
+        return "miu_timetable_bot";
+    }
+
+    public String getBotToken() {
+        return "703879629:AAHG_8cd7B0j3cUomvjnrFJzHiyXKHgrWJE";
+    }
+}
